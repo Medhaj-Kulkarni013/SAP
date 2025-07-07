3f2e1d17495e5c23c288430a9e64d19f15345ace
Medhaj Kulkarni <medhkulkarni.ext@deloitte.com>
2025-07-07 06:05:09 +0000

    Flask app.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SAP
+This is a trial repo for deployment of a program on SAP Cloud Foundry
--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World from Python on Cloud Foundry!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
