cb0100dfb6ccd5dd4e617a462c208109a2c788c0
Medhaj Kulkarni <medhkulkarni.ext@deloitte.com>
2025-07-08 10:18:19 +0000

    blah

---
+++ move_to_new_subacc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ------------------ CONFIG ------------------
+API_ENDPOINT="https://api.cf.us10-001.hana.ondemand.com"  
+ORG="ee55f632trial_test-z2ow5x2b"                                  
+SPACE="dev"
+APP_NAME="hello-python"
+APP_DIR="." 
+# --------------------------------------------
+
+echo "Logging into Cloud Foundry..."
+cf login -a $API_ENDPOINT
+
+echo "Targeting org and space..."
+cf target -o "$ORG" -s "$SPACE"
+
+echo "Deploying app: $APP_NAME"
+cd "$APP_DIR"
+cf push "$APP_NAME"
